f36d4b0d6e38fe8e5955a8b89f6285c3a93695c4
Fanny Jiang <jiangf@bu.edu>
2017-03-06 10:40:49 -0500

    played around with timeout

--- bash.js
+++ bash.js
@@ -6,20 +6,8 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-  var commands = require('./command');
-  commands[cmd](cmd);
-
-  // if (cmd === "pwd") {
-  //   // process.stdout.write(process.env.PWD); // this works too!
-  //   process.stdout.write(process.cwd());
-  //   process.stdout.write('\nprompt > ');
-  // }
-
-  if (cmd === "date") {
-    process.stdout.write(Date().toString());
-    process.stdout.write('\nprompt > ');
-  }
-  // process.stdout.write('You typed: ' + cmd);
-  // process.stdout.write('\nprompt > ');
+  var commands = require('./command'); // commands is an object
+  commands[cmd](cmd); // cmd is a property on commands and we also called our passed in cmd on this property
 
 });
+
--- command.js
+++ command.js
@@ -2,23 +2,42 @@ module.exports = {
     pwd: function(cmd) {
         if (cmd === "pwd") {
          // process.stdout.write(process.env.PWD); // this works too!
-        process.stdout.write(process.cwd());
-        process.stdout.write('\nprompt > ');
-  }
+            process.stdout.write(process.cwd());
+            process.stdout.write('\nprompt > ');
+        }
     },
+
+    date: function(cmd) {
+        if (cmd === "date") {
+            process.stdout.write(Date().toString());
+            process.stdout.write('\nprompt > ');
+        }
+    },
+
     ls: function(cmd){
-        if(cmd === 'ls'){
+        if (cmd === 'ls'){
             var fs = require('fs');
-            process.stdout.write('You typed ls.');
             fs.readdir('.', function(err, files) {
                 if (err) throw err;
                 files.forEach(function(file) {
                 process.stdout.write(file.toString() + "\n");
-                });
             });
-                process.stdout.write("prompt > ");
+                process.stdout.write('\nprompt > ');
+            });
             }
+        },
+
+    timeout: function(cmd) {
+        if (cmd === 'timeout') {
+            var startTime = new Date;
+
+            setTimeout(function () { // callback functions don't stop your program
+                var endTime = new Date;
+                console.log('Time elapsed: ', endTime - startTime, 'ms');
+            }, 500);
+
+            while (new Date - startTime < 250) {}; // this happens first
         }
-    }
 
-
+        }
+    };
